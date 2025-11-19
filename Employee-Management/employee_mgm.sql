ebe858bf9d0b331bc0cd7f367d40365d044db4f8
AnishAwalgaonkar <awalgaonkar.anish88@gmail.com>
2025-11-19 09:45:22 +0530

    Add files via upload

---
+++ Employee-Management/employee_mgm.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE employeemanagement;
+USE employeemanagement;
+CREATE TABLE login(username varchar(20), password varchar(20));
+INSERT INTO login VALUES('Anish', '160320');
+SELECT * FROM LOGIN;
+
+CREATE TABLE employee(name varchar(40), fname varchar(40), dob varchar(40), salary varchar(40), address varchar(40), phone varchar(40), email varchar(40), education varchar(40), designation varchar(40), aadhar varchar(40), empID varchar(40) );
+SELECT * FROM employee
